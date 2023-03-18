051e135404eb0726639440485e58c814a3fa2e7b
Kopa_From_PC <paucullaloren@gmail.com>
2023-03-19 00:22:07 +0100

    The counter has been updated
    Now it's starting to work

--- js/counter.js
+++ js/counter.js
@@ -1,24 +1,37 @@
 class Counter {
     constructor() {
         // Seleccionamos el elemento HTML que queremos actualizar
-        this.counter = document.getElementById('counter');
+        this.counter = document.getElementById("counter");
 
+        // Se establece el tiempo total del ciclo:
         this.timeToElapse = 1500;
-    }
 
-    updateCounter() {
-        const minutos = Math.floor(timeToElapse / 60);
-        const segundos = timeToElapse % 60;
-        this.timeToElapse--;
+        // Se extrae el número de minutos restantes (de los milisegundos restantes):
+        this.minutes = Math.floor(this.timeToElapse / 60);
+        // Se extrae el número de segundos restantes (de los milisegundos restantes):
+        this.seconds = this.timeToElapse % 60;
     }
 
     showCurrentTime() {
-        this.contador.innerHTML = `${minutos.toString().padStart(2, '0')}:${segundos.toString().padStart(2, '0')}`;
+        this.counter.innerHTML = `${this.minutes.toString().padStart(2, '0')}:${this.seconds.toString().padStart(2, '0')}`;
+    }
+
+    updateCounter() {
+        if (this.seconds == 0) {
+            this.minutes -= 1;
+            this.seconds = 59;
+        } else {
+            // Se resta un segundo al tiempo restante:
+            this.seconds -= 1;
+        }
+
+        // Se llama al método que muestra el tiempo restante en pantalla:
+        this.showCurrentTime();
     }
 }
 
 // Se crea un objeto de la clase Counter:
 const counter = new Counter()
 
-// Se llama al método updateCounter cada segundo.
-setInterval(counter.updateCounter, 1000);
+// Se llama al método updateCounter cada segundo mediante una función flecha:
+setInterval(() => { counter.updateCounter() }, 1000);
