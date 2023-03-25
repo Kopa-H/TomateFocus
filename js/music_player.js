744b538c6178b92f85367ab554b45e360f28f053
Kopa_From_Mac <paucullaloren@gmail.com>
2023-03-25 19:55:07 +0100

    Adding new songs to the music player

--- js/music_player.js
+++ js/music_player.js
@@ -1,7 +1,9 @@
 class MusicPlayer {
     constructor() {
-        this.song = document.querySelector(".music-player-song");
+        this.song1 = document.querySelector(".music-player-song");
+        this.song2 = document.querySelector(".music-player-song-2");
         this.playButton = document.querySelector(".music-player-play-and-pause-button");
+        this.nextSongButton = document.querySelector(".music-next-song-button")
         this.volumeSlider = document.querySelector(".volume-slider")
 
         this.songIsPlaying = false;
@@ -9,18 +11,27 @@ class MusicPlayer {
         this.playButton.addEventListener('click', () => {
             if (!this.songIsPlaying) {
               console.log("The song starts!");
-              this.song.play();
+              this.song1.play();
               this.songIsPlaying = true;
             } else {
               console.log("The song stops!");
-              this.song.pause();
+              this.song1.pause();
               this.songIsPlaying = false;
             }
         });
 
-        this.volumeSlider.addEventListener('input', () => {
-          this.song.volume = this.volumeSlider.value;
+        this.nextSongButton.addEventListener('click', () => {
+          this.changeToNextSong()
         });
+
+        this.volumeSlider.addEventListener('input', () => {
+          this.song1.volume = this.volumeSlider.value;
+        });
+    }
+
+    changeToNextSong() {
+      this.song1.pause();
+      this.song2.play();
     }
 }
 
