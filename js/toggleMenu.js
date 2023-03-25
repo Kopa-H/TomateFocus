c8289f432e485260d6183c4a6326f15e614ce5e8
Patrick Feeney <patrickfeeneytamayo@gmail.com>
2023-03-25 20:09:18 +0000

    Improve code toggleMenu.js
    Changes 'let' to 'const'.
    Also, instead of using an if statement now there is a ternary
    operator.
    Also, the onclick function is now an anonymous function.
    It was called 'funciton' before. But it never had to get
    directly called. It only gets called by the onclick.

--- js/toggleMenu.js
+++ js/toggleMenu.js
@@ -1,19 +1,15 @@
 function toggleMenu() {
-    var element = document.getElementById("toggleMenu");
-    var elementStyles = window.getComputedStyle(element)
-    var displayType = elementStyles.display
+    const element = document.getElementById("toggleMenu");
+    const elementStyles = window.getComputedStyle(element)
+    const displayType = elementStyles.display
     console.log(displayType)
     
-    if(displayType === "none") {
-        element.style.display = "flex"
-    }
-    else {
-        element.style.display = "none"
-    }    
+    // Ternary Operator
+    element.style.display = displayType === "none" ? "flex" : "none"
 }
 
 // This function works perfectly. It's very simple too.
-let gear = document.getElementById("settings-icon")
-gear.onclick = function() {
+const gear = document.getElementById("settings-icon")
+gear.onclick = () => {
     toggleMenu()
 }
