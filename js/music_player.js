5188264309b1cc717730a489154202c6a9e08bc0
Kopa_From_Mac <paucullaloren@gmail.com>
2023-03-25 11:37:30 +0100

    The style of the musicPlayer has been adjusted.

--- js/music_player.js
+++ js/music_player.js
@@ -1,20 +1,25 @@
 class MusicPlayer {
     constructor() {
         this.song = document.querySelector(".music-player-song");
-        this.playButton = document.querySelector(".music-player-play-button");
+        this.playButton = document.querySelector(".music-player-play-and-pause-button");
+        this.volumeSlider = document.querySelector(".volume-slider")
 
         this.songIsPlaying = false;
 
         this.playButton.addEventListener('click', () => {
-            if (this.songIsPlaying) {
-              console.log("En teoría se detiene la canción!");
-              this.song.pause();
-              this.songIsPlaying = false;
-            } else {
-              console.log("En teoría se empieza a reproducir la canción!");
+            if (!this.songIsPlaying) {
+              console.log("The song starts!");
               this.song.play();
               this.songIsPlaying = true;
+            } else {
+              console.log("The song stops!");
+              this.song.pause();
+              this.songIsPlaying = false;
             }
+        });
+
+        this.volumeSlider.addEventListener('input', () => {
+          this.song.volume = this.volumeSlider.value;
         });
     }
 }
