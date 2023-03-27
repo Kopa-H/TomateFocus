847f8a8bea088dc6b521039016c4d871fe231da3
Patrick Feeney <patrickfeeneytamayo@gmail.com>
2023-03-27 15:54:16 +0100

    Change time for pomo, short and long

--- js/toggleMenu.js
+++ js/toggleMenu.js
@@ -17,6 +17,11 @@ gear.onclick = () => {
 
 const pomoMinus = document.querySelector(".pomodoro .minus-button")
 const pomoPlus = document.querySelector(".pomodoro .plus-button")
+const shortMinus = document.querySelector(".short-break .minus-button")
+const shortPlus = document.querySelector(".short-break .plus-button")
+const longMinus = document.querySelector(".long-break .minus-button")
+const longPlus = document.querySelector(".long-break .plus-button")
+
 pomoMinus.addEventListener('click', function () {
     let textMinutes = document.querySelector(".pomodoro .menu-text")
     // the match(/\d+/g) is a regex. It matches numbers
@@ -25,7 +30,6 @@ pomoMinus.addEventListener('click', function () {
 
     // Call the function that is inside counter.js
     callChangeDuration("pomodoro", textMinutesValue*60)
-    console.log(textMinutesValue)
 })
 pomoPlus.addEventListener('click', function () {
     let textMinutes = document.querySelector(".pomodoro .menu-text")
@@ -34,6 +38,38 @@ pomoPlus.addEventListener('click', function () {
 
     callChangeDuration("pomodoro", textMinutesValue*60)
 })
+shortMinus.addEventListener("click", function () {
+    let textMinutes = document.querySelector(".short-break .menu-text")
+    let textMinutesValue = +textMinutes.textContent.match(/\d+/g)
+    textMinutes.textContent = `${--textMinutesValue} minutes`
+
+    callChangeDuration("shortbreak", textMinutesValue*60)
+})
+shortPlus.addEventListener("click", function () {
+    let textMinutes = document.querySelector(".short-break .menu-text")
+    let textMinutesValue = +textMinutes.textContent.match(/\d+/g)
+    textMinutes.textContent = `${++textMinutesValue} minutes`
+
+    callChangeDuration("shortbreak", textMinutesValue*60)
+})
+longMinus.addEventListener("click", function () {
+    let textMinutes = document.querySelector(".long-break .menu-text")
+    let textMinutesValue = +textMinutes.textContent.match(/\d+/g)
+    textMinutes.textContent = `${--textMinutesValue} minutes`
+
+    callChangeDuration("longbreak", textMinutesValue*60)
+})
+longPlus.addEventListener("click", function () {
+    let textMinutes = document.querySelector(".long-break .menu-text")
+    let textMinutesValue = +textMinutes.textContent.match(/\d+/g)
+    textMinutes.textContent = `${++textMinutesValue} minutes`
+
+    callChangeDuration("longbreak", textMinutesValue*60)
+})
+
+
+
+
 
 
 function callChangeDuration(timerType, time) {
