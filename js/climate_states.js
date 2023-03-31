9c0f444a9c49293d5a8e0ba8f342200a341f9dc9
Kopa_From_PC <paucullaloren@gmail.com>
2023-03-31 18:22:51 +0200

    The climate states system has been finished

--- js/climate_states.js
+++ js/climate_states.js
@@ -5,34 +5,60 @@ class ClimateStates {
       this.forestButton = document.querySelector(".forest-state-button");
 
       this.fireplaceSound = document.querySelector(".fireplace-sound");
+      this.fireplaceSound.volume = 0.5;
       this.rainSound = document.querySelector(".rain-sound");
+      this.rainSound.volume = 0.05;
       this.forestSound = document.querySelector(".forest-sound");
+      this.forestSound.volume = 0.05;
+
+      this.fireplaceSoundBeingPlayed = false
+      this.rainSoundBeingPlayed = false
+      this.forestSoundBeingPlayed = false
 
       this.fireplaceButton.addEventListener('click', () => {
-        this.playFireplaceSounds();
+        if (this.fireplaceSoundBeingPlayed == false) {
+          this.playSoundInLoop(this.fireplaceSound);
+          this.fireplaceButton.style.backgroundColor = "rgb(125, 45, 126)";
+          this.fireplaceSoundBeingPlayed = true;
+        } else {
+          this.fireplaceSound.pause();
+          this.fireplaceButton.style.backgroundColor = "gray";
+          this.fireplaceSoundBeingPlayed = false;
+        }
       });
+
       this.rainButton.addEventListener('click', () => {
-        this.playRainSounds();
+        if (this.rainSoundBeingPlayed == false) {
+          this.playSoundInLoop(this.rainSound);
+          this.rainButton.style.backgroundColor = "rgb(125, 45, 126)";
+          this.rainSoundBeingPlayed = true;
+        } else {
+          this.rainSound.pause();
+          this.rainButton.style.backgroundColor = "gray";
+          this.rainSoundBeingPlayed = false;
+        }
       });
+
       this.forestButton.addEventListener('click', () => {
-        this.playForestSounds();
+        if (this.forestSoundBeingPlayed == false) {
+          this.playSoundInLoop(this.forestSound);
+          this.forestButton.style.backgroundColor = "rgb(125, 45, 126)";
+          this.forestSoundBeingPlayed = true;
+        } else {
+          this.forestSound.pause();
+          this.forestButton.style.backgroundColor = "gray";
+          this.forestSoundBeingPlayed = false;
+        }
       });
     };
 
-    playFireplaceSounds() {
-        console.log("Se cambia al estado climático de Fireplace");
-        this.fireplaceSound.play();
+    playSoundInLoop(sound) {
+      sound.addEventListener('ended', function() {
+        this.currentTime = 0;
+        this.play();
+      }, false);
+      sound.play();
     };
-
-    playRainSounds() {
-        console.log("Se cambia al estado climático de Rain");
-        this.rainSound.play();
-    };
-
-    playForestSounds() {
-        console.log("Se cambia al estado climático de Forest");
-        this.forestSound.play();
-    };
-}
+};
 
 const climateStates = new ClimateStates();
