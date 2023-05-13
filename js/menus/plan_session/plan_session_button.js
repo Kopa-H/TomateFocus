389ac96af4287f11e79d34b2429e3468c4685c7c
Kopa_From_PC <paucullaloren@gmail.com>
2023-05-13 23:37:56 +0200

    The plan session menu is being improved

---
+++ js/menus/plan_session/plan_session_button.js
@@ -0,0 +1,39 @@
+class PlanSessionButton {
+
+    constructor() {
+        this.planSessionButton = document.querySelector('.plan-session-button');
+        this.planSessionDescription = document.querySelector('.plan-session-description');
+        this.planSession = document.querySelector('.plan-session-zone');
+        this.planSessionStyles = window.getComputedStyle(this.planSession);
+
+        // Para mostrar la descripción:
+        this.planSessionButton.addEventListener('mouseover', () => {
+            this.planSessionDescription.style.opacity = '0.8';
+        });
+        this.planSessionButton.addEventListener('mouseleave', () => {
+            this.planSessionDescription.style.opacity = '0';
+        });
+
+        this.planSessionButton.addEventListener('click', () => {;
+            this.togglePlanSession();
+        });
+	}
+
+    togglePlanSession() {
+        if (this.planSessionStyles.display == "none") {
+            this.planSession.classList.remove("slideOut");
+            this.planSession.classList.add("slideIn");
+            this.planSession.style.display = "flex";
+        } else {
+            this.planSession.classList.remove("slideIn");
+            this.planSession.classList.add("slideOut");
+
+            this.planSession.addEventListener("animationend", () => {
+                this.planSession.style.display = "none"
+            }, { once: true })
+        }
+    }
+}
+
+window.PlanSessionButton = PlanSessionButton;
+const planSessionButton = new PlanSessionButton();
