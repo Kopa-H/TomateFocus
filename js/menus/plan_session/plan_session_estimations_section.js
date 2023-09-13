b2b2ddd5e174a12fed46584aeeefeb540b092ce5
Kopa_From_PC <paucullaloren@gmail.com>
2023-09-13 11:34:11 +0200

    The feature is being created

--- js/menus/plan_session/plan_session_estimations_section.js
+++ js/menus/plan_session/plan_session_estimations_section.js
@@ -4,6 +4,47 @@ class PlanSessionEstimations {
         this.studyTimeEstimation = document.querySelector('.plan-session-estimations-section-study-time');
         this.breakTimeEstimation = document.querySelector('.plan-session-estimations-section-break-time');
         this.totalTimeEstimation = document.querySelector('.plan-session-estimations-section-total-time');
+
+        this.pomodoroFunction = () => {this.runPomodoro()};
+        this.shortBreakFunction = () => {this.runShortBreak()};
+        this.longBreakFunction = () => {this.runLongBreak()};
+
+        this.numberOfPomodoroCycles = 0;
+        this.numberOfShortBreakCycles = 0;
+        this.numberOfLongBreakCycles = 0;
+
+        // Se obtiene el itinerario actual
+        logicHandler.itineraryList.forEach((element, index) => {
+
+            // Define una función personalizada para comparar las funciones
+            const areEqualFunctions = (fn1, fn2) => fn1.toString() === fn2.toString();
+
+            if (areEqualFunctions(element, this.pomodoroFunction)) {
+                console.log("ciclo pomodoro");
+                this.numberOfPomodoroCycles++
+
+            } else if (areEqualFunctions(element, this.shortBreakFunction)) {
+                console.log("ciclo shortBreak");
+                this.numberOfShortBreakCycles++
+
+            } else if (areEqualFunctions(element, this.longBreakFunction)) {
+                console.log("ciclo longBreak");
+                this.numberOfLongBreakCycles++
+
+            } else {
+                console.log("Unknown cycle")};
+
+        });
+
+        this.pomodoroTime = (this.numberOfPomodoroCycles * logicHandler.pomodoroTimeToElapse) / 60;
+        this.pomodoroTimeToHours = `${Math.floor(this.pomodoroTime / 60)}h ${Math.floor(this.pomodoroTime % 60)}min`;
+
+        this.breakTime = ((this.numberOfShortBreakCycles * logicHandler.shortbreakTimeToElapse) / 60) + ((this.numberOfLongBreakCycles * logicHandler.longbreakTimeToElapse) / 60);
+        this.breakTimeToHours = `${Math.floor(this.breakTime / 60)}h ${Math.floor(this.breakTime % 60)}min`;
+
+        this.studyTimeEstimation.innerHTML = this.pomodoroTimeToHours;
+        this.breakTimeEstimation.innerHTML = this.shortBreakTimeToHours;
+        this.totalTimeEstimation.innerHTML = this.longBreakTimeToHours;
 	}
 }
 
