24f0edef4634d0a758953fdda6c066b8686671a4
Kopa_From_PC <paucullaloren@gmail.com>
2023-09-15 21:09:26 +0200

    Trying to add the session end confetti
    I wasted the entire afternoon

--- js/session_ending.js
+++ js/session_ending.js
@@ -1,6 +1,6 @@
 class SessionEnding {
     constructor() {
-
+        this.isSessionEnded = false;
     }
 
     endTheSession() {
@@ -9,6 +9,8 @@ class SessionEnding {
         audioHandler.sessionEndingMusic.play();
         this.cleanInterface();
         this.showSessionStatistics();
+
+        this.showConfetti();
     }
 
     cleanInterface() {
@@ -41,6 +43,19 @@ class SessionEnding {
         console.log(`God Mode Usages: ${"placeholder"}`);
 
         console.log("Congratulations! You just finished your study session. Remebember that a great student is that who can also enjoy life, and if you are currently fighting a deadline do whatever you can, but don't forget to take care of yourself and your anxiety.")
+    }
+
+    launchConfettiExplosion() {
+        let confettiContainer = document.querySelector("confetti-container");
+        let confetti_zone = confetti.create(confettiContainer);
+        confetti_zone().then(() => {});
+    }
+
+    // Función para mostrar el confeti
+    showConfetti() {
+        setInterval(() => {
+            this.launchConfettiExplosion(); // Llamar a la función para mostrar el confeti
+        }, 1000); // Mostrar confeti cada 5 segundos (ajusta el intervalo según tus necesidades)
     }
 }
 
