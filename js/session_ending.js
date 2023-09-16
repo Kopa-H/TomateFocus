6d67f303b13955b3fd147a0eb80c661ea7178f27
Kopa_From_PC <paucullaloren@gmail.com>
2023-09-16 23:26:39 +0200

    Now the session end part is ready to be finished

--- js/session_ending.js
+++ js/session_ending.js
@@ -3,7 +3,8 @@ class SessionEnding {
         this.isSessionEnded = false;
         this.confettiCanvas = document.getElementById("confetti-canvas");
 
-        this.centralZone = document.querySelector(".central-zone-wrapper")
+        this.centralZone = document.querySelector(".central-zone-wrapper");
+        this.statisticsContainer = document.querySelector('.statistics-container');
     }
 
     endTheSession() {
@@ -58,6 +59,18 @@ class SessionEnding {
         console.log(`God Mode Usages: ${"placeholder"}`);
 
         console.log("Congratulations! You just finished your study session. Remebember that a great student is that who can also enjoy life, and if you are currently fighting a deadline do whatever you can, but don't forget to take care of yourself and your anxiety.")
+
+        // Crea un nuevo párrafo
+        const newParagraph1 = document.createElement('p');
+        newParagraph1.textContent = 'Este es el primer párrafo generado desde JavaScript.';
+
+        // Crea otro párrafo
+        const newParagraph2 = document.createElement('p');
+        newParagraph2.textContent = 'Este es el segundo párrafo generado desde JavaScript.';
+
+        // Agrega los párrafos al 'statistics-container'
+        this.statisticsContainer.appendChild(newParagraph1);
+        this.statisticsContainer.appendChild(newParagraph2);
     }
 
         // Función para mostrar el confeti
